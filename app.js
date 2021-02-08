d98b029da8f1d685a5af06d01e893d5ea724b4e6
Balaji Durai Masanam <baladurai420@gmail.com>
2021-02-08 11:27:03 +0530

    removed / from app routers

--- app.js
+++ app.js
@@ -38,10 +38,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/pretest', pretestRouter);
-app.use('/posttest', posttestRouter);
+app.use('', indexRouter);
+app.use('users', usersRouter);
+app.use('pretest', pretestRouter);
+app.use('posttest', posttestRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
